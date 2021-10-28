6f8c662000a89238ba5accbc6cb1aa4902c8e0db
magisystem0408 <masato.matsudo@gmail.com>
2021-10-28 20:48:38 +0900

    commit

---
+++ lib/screens/add_user_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddUserPage extends StatefulWidget {
+  const AddUserPage({key}) : super(key: key);
+
+  @override
+  _AddUserPageState createState() => _AddUserPageState();
+}
+
+class _AddUserPageState extends State<AddUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
